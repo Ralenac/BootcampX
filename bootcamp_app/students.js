a642e21daa36a749b17c12664cc005a87089a032
Ralenac <t.ralenac@gmail.com>
2021-10-13 21:56:09 -0400

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -12,13 +12,13 @@ const limit = process.argv[3] || 5;
 // Store all potentially malicious values in an array.
 const values = [`%${cohortName}%`, limit];
 
-pool.query(`
+const queryString =`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE $1
 LIMIT $2;
-`)
+`
 
 pool.query(queryString, values)
 .then(res => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,9 +8,9 @@ const pool = new Pool({
 });
 
 
-const cohortName = process.argv[2] || 'JUL02';
+const cohortName = [`${process.argv[2]}` || 'JUL02'];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
@@ -18,7 +18,7 @@ JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
 
 pool.query(queryString, cohortName)
 .then(res => {
